ab76306e68c752b78a9d77c0c673304af4523c2a
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-06-02 23:21:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,38 +8,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 
-# Train data transformations
-train_transforms = transforms.Compose([
-    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
-    transforms.Resize((28, 28)),
-    transforms.RandomRotation((-15., 15.), fill=0),
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,)),
-    ])
-
-# Test data transformations
-test_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-    ])
-
-train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
-test_data = datasets.MNIST('../data', train=False, download=False, transform=test_transforms)
-batch_size = 512
-
-kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 1, 'pin_memory': True}
 
 
-train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
-test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
-
-# Data to plot accuracy and loss graphs
-train_losses = []
-test_losses = []
-train_acc = []
-test_acc = []
-
-test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
 
 from tqdm import tqdm
 
